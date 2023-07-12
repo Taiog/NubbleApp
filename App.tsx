085b407b0b8d8147c5130a0c0e6c703dc50d8eeb
Tiago <tiagoargollo@hotmail.com>
2023-07-12 18:51:42 -0300

    screen component

--- App.tsx
+++ App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
-import {Button} from './src/components/Button/Button';
-import {Icon} from './src/components/Icon/Icon';
+import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
 
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <View style={{paddingHorizontal: 24}}>
-          <Text preset="paragraphLarge" italic>
-            CoffStack
-          </Text>
-          <Button title="Entrar" marginBottom="s12" />
-          <Icon name="camera" size={50} color="buttonPrimary" />
-        </View>
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        {/* <LoginScreen /> */}
+        <SignUpScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
